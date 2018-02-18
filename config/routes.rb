a5d78b19b2862eff36beb44391a7ea6bb1dde37b
tanyildiz <tanyildizn@gmail.com>
2018-02-18 12:14:29 +0300

    tags, products, users, categories controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'products/create'
+
+  get 'products/new'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
+  get 'products/show'
+
+  get 'products/index'
+
+  get 'users/create'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'users/show'
+
+  get 'users/index'
+
+  get 'categories/create'
+
+  get 'categories/new'
+
+  get 'categories/edit'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
   get 'home/login'
 
   get 'home/admin'
@@ -19,9 +61,14 @@ Rails.application.routes.draw do
 
   get 'tags/index'
 
-  resources :homes
+  resources :tags
+  resources :products
+  resources :users
+  resources :categories
   get 'home/index'
-
+  get 'home' => 'home#index'
+  get 'contact'=> 'home#contact'
+  get 'login' => 'home#login'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
